f0e156fd5167ee713a12af9b0e8d63ca27ee98c8
rymnd0800 <raymundregalado8@gmail.com>
2023-10-27 00:38:55 +0800

    update dashboard, move env folder

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   userEmail?: string
-
+  constructor(private firebase: FirebaseService, private router: Router) {
+    
+  }
   ngOnInit(): void {
    const currentUser = JSON.parse(window.sessionStorage.getItem('userInfo')!)
     this.userEmail = currentUser.email
+  }
+
+  logout(){
+    this.firebase.signOut()
+    .then(res =>{
+      this.router.navigate(['/login'])
+    })
+    .catch(error=>{
+      console.log(error)
+    })
   }
 
 
